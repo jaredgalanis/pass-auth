a38a731171df4554440692dd7ecb546c8ab4a738
John Abrahams <jabrah20@jhu.edu>
2022-11-04 09:53:47 -0400

    Comment out a proxyReq for old User Service

--- config/proxies.js
+++ config/proxies.js
@@ -19,31 +19,37 @@ module.exports = function (app, apiProxy, config) {
 
   /////////////////////////////////// PROXIED SERVICES IN PASS DOCKER ///////////////////////////////
 
-  apiProxy.on('proxyReq', function (proxyReq, req) {
-    const user = req.user;
+  /**
+   * This 'proxyReq' section was intended for use with the User Service
+   * Commented out for now until the service is ported to the new v2 infra,
+   * at which point it should be properly scoped to the service's route
+   * The mapping may also be helpful for the future port/impl
+   */
+  // apiProxy.on('proxyReq', function (proxyReq, req) {
+  //   const user = req.user;
 
-    proxyReq.setHeader('Displayname', user.shibbolethAttrs.displayName);
-    proxyReq.setHeader('Mail', user.shibbolethAttrs.email);
-    proxyReq.setHeader('Eppn', user.shibbolethAttrs.eppn);
-    proxyReq.setHeader('Givenname', user.shibbolethAttrs.givenName);
-    proxyReq.setHeader('Sn', user.shibbolethAttrs.surname);
-    proxyReq.setHeader('Affiliation', user.shibbolethAttrs.scopedAffiliation);
-    proxyReq.setHeader('Employeenumber', user.shibbolethAttrs.employeeNumber);
-    proxyReq.setHeader('unique-id', user.shibbolethAttrs.uniqueId);
-    proxyReq.setHeader('employeeid', user.shibbolethAttrs.employeeIdType);
+  //   proxyReq.setHeader('Displayname', user.shibbolethAttrs.displayName);
+  //   proxyReq.setHeader('Mail', user.shibbolethAttrs.email);
+  //   proxyReq.setHeader('Eppn', user.shibbolethAttrs.eppn);
+  //   proxyReq.setHeader('Givenname', user.shibbolethAttrs.givenName);
+  //   proxyReq.setHeader('Sn', user.shibbolethAttrs.surname);
+  //   proxyReq.setHeader('Affiliation', user.shibbolethAttrs.scopedAffiliation);
+  //   proxyReq.setHeader('Employeenumber', user.shibbolethAttrs.employeeNumber);
+  //   proxyReq.setHeader('unique-id', user.shibbolethAttrs.uniqueId);
+  //   proxyReq.setHeader('employeeid', user.shibbolethAttrs.employeeIdType);
 
-    // this is necessary because the bodyParser middleware and http-proxy
-    // do not play well together with POST requests
-    // https://github.com/http-party/node-http-proxy/issues/180
-    if (req.body) {
-      let bodyData = JSON.stringify(req.body);
-      // incase if content-type is application/x-www-form-urlencoded -> we need to change to application/json
-      proxyReq.setHeader('Content-Type', 'application/json');
-      proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
-      // stream the content
-      proxyReq.write(bodyData);
-    }
-  });
+  //   // this is necessary because the bodyParser middleware and http-proxy
+  //   // do not play well together with POST requests
+  //   // https://github.com/http-party/node-http-proxy/issues/180
+  //   if (req.body) {
+  //     let bodyData = JSON.stringify(req.body);
+  //     // incase if content-type is application/x-www-form-urlencoded -> we need to change to application/json
+  //     proxyReq.setHeader('Content-Type', 'application/json');
+  //     proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+  //     // stream the content
+  //     proxyReq.write(bodyData);
+  //   }
+  // });
 
   app.all('/fcrepo/*', ensureAuthenticated, function (req, res) {
     const base64Creds = Buffer.from(
